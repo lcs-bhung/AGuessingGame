4688f31762a72a5fc2777e8efa7c6557bb5b410a
Branton Hung <BHung@lcs.on.ca>
2019-09-30 11:17:32 -0400

    Initial Commit

---
+++ AGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGuessingGame
+//
+//  Created by Branton Hung on 2019-09-30.
+//  Copyright © 2019 Branton Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
